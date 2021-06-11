b11712e8aec0106679ef3af4318fb83ca9bcc5e7
Vitor Franco <vitoroliveirafranco@gmail.com>
2021-06-11 09:17:16 -0300

    adding configs of swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+import { Request, Response } from "express";
 
 import { usersRoutes } from "./routes/users.routes";
+
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export { app };
